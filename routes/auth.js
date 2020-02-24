7f3b849191a273934cdc5998b9f1aad0b8baada3
Ransaka Ravihara <ransakaravi@icloud.com>
2020-02-25 00:47:35 +0530

    user post mrthos implimented

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,19 @@
 const router  = require('express').Router();
+const User = require('../models/User')
 
 router.post('/register',(req,res)=>{
-     res.send('Register');
+    console.log(req.body)
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    })
+    try{
+        const savedUser = user.save();
+        res.send("User saved");
+    }catch(err){
+        res.status(400).send(err)
+    }
 });
 
 
