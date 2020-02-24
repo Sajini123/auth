cce12d37e4668c694e7c8548f41fd9d2c690a19c
Ransaka Ravihara <ransakaravi@icloud.com>
2020-02-25 01:29:03 +0530

    register validation completed

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,15 @@
 const router  = require('express').Router();
 const User = require('../models/User')
+const {registerValidation,loginValidation} = require('../validation');
+
 
 router.post('/register',(req,res)=>{
-    console.log(req.body)
+    
+    // validation
+    const {error} = registerValidation(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+
+    // console.log(req.body)
     const user = new User({
         name: req.body.name,
         email: req.body.email,
