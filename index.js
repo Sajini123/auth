35dc023e92d5220479c3d8e7779e2cdea3543e95
Ransaka Ravihara <ransakaravi@icloud.com>
2020-02-24 22:38:51 +0530

    server created

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const app = express();
 
-app.listen(port, () => console.log(`listening on http://localhost:${port}`));
+// import routes 
+const authRoute = require('./routes/auth');
+
+// route middlewares
+app.use('api/user',authRoute);
+app.listen(3000, () => console.log(`listening on port 3000`));
